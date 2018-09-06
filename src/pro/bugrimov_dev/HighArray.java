999168d17bff417c7d337a2ef74c966e5aa0adeb
Александр Бугримов <bugrimov_a@bk.ru>
2018-09-07 00:45:06 +0300

    Change class HighArray

--- src/pro/bugrimov_dev/HighArray.java
+++ src/pro/bugrimov_dev/HighArray.java
@@ -1,4 +1,53 @@
 package pro.bugrimov_dev;
 
 public class HighArray {
+
+    private long[] arr;
+
+    private int nElements;
+
+    public HighArray(final int max) {
+        arr = new long[max];
+        nElements = 0;
+    }
+
+    /**
+     * Поиск
+     * @param searchKey значение
+     * @return найден/не найден
+     */
+    public boolean find(long searchKey) {
+        int j;
+        for (j = 0; j < nElements; j++) {
+            if (arr[j] == searchKey)
+                break;
+        }
+        if (j == nElements) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void insert(long value) {
+        arr[nElements] = value;
+        nElements++;
+    }
+
+    public boolean delete(long value) {
+        int j;
+        for (j = 0; j < nElements; j++) {
+            if (value == arr[j])
+                break;
+        }
+        if (j == nElements) {
+            return false;
+        } else {
+            for (int k = j; k < nElements; k++) {
+                arr[k] = arr[k + 1];
+            }
+            nElements--;
+            return true;
+        }
+    }
 }
